1bc960d87be363495be14012f00b05a7c730c2f4
JersonMraz <jersonsullano201@gmail.com>
2025-04-09 22:32:32 +0800

    gi adjust nako ang logout button sa imo dashboard ya butngan nakog sweetalert2

--- connection.php
+++ connection.php
@@ -1,16 +1,12 @@
 <?php
 
-$db_server = '127.0.0.1';
+$db_server = '127.0.0.1:3307';
 $db_user = 'root';
 $db_pass = "";
 $db_name = 'nabuadb';
 
 $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-
-/*check kung mo gana ba ang database*/
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-?>
